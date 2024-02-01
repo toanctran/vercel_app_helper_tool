b31ce47c26ea08056ec1a2ad16819e6e8dcf64cb
TOAN TRAN <chitoantran@outlook.com>
2024-02-01 13:19:47 +0700

    update app

--- app.py
+++ app.py
@@ -106,30 +106,32 @@ def get_summary(request_data: SummaryRequestData):
     """
     Endpoint to summarize give content
     """
+    try:
+        llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-1106", openai_api_key = os.environ["OPENAI_API_KEY"])
+        text_splitter = RecursiveCharacterTextSplitter(
+            separators=["\n\n", "\n"], chunk_size=10000, chunk_overlap=500)
+        docs = text_splitter.create_documents([request_data.content])
+        map_prompt = """
+        Write a detailed summary of the following text for a research purpose:
+        "{text}"
+        SUMMARY:
+        """
+        map_prompt_template = PromptTemplate(
+            template=map_prompt, input_variables=["text"])
 
-    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-1106", openai_api_key = os.environ["OPENAI_API_KEY"])
-    text_splitter = RecursiveCharacterTextSplitter(
-        separators=["\n\n", "\n"], chunk_size=10000, chunk_overlap=500)
-    docs = text_splitter.create_documents([request_data.content])
-    map_prompt = """
-    Write a detailed summary of the following text for a research purpose:
-    "{text}"
-    SUMMARY:
-    """
-    map_prompt_template = PromptTemplate(
-        template=map_prompt, input_variables=["text"])
+        summary_chain = load_summarize_chain(
+            llm=llm,
+            chain_type='map_reduce',
+            map_prompt=map_prompt_template,
+            combine_prompt=map_prompt_template,
+            verbose=True
+        )
 
-    summary_chain = load_summarize_chain(
-        llm=llm,
-        chain_type='map_reduce',
-        map_prompt=map_prompt_template,
-        combine_prompt=map_prompt_template,
-        verbose=True
-    )
+        output = summary_chain.run(input_documents=docs,)
 
-    output = summary_chain.run(input_documents=docs,)
-
-    return {"summary_text":output}
+        return {"summary_text":output}
+    except Exception as e:
+        return {"error": str(e)}
 
 
 
